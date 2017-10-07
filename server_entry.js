d8b9430d34df2ecf08c3bb63109f9262210d5a5d
sokin1 <sokin1@hotmail.com>
2017-10-07 15:11:43 -0400

    firebase admin introduced

--- server_entry.js
+++ server_entry.js
@@ -1,17 +1,13 @@
 var net = require('net')
 import decoder from './Decoder'
 
-var firebase = require('firebase')
+var admin = require('firebase-admin')
+var serviceAccount = require('./resource/userinfo-7f8f9-firebase-adminsdk-ophcs-42fb689ca1.json')
 
-var config = {
-    apiKey: "AIzaSyAp9WiJPvArikLgXJQmJg5Kn7OJ2hoDv60",
-    authDomain: "userinfo-7f8f9.firebaseapp.com",
-    databaseURL: "https://userinfo-7f8f9.firebaseio.com",
-    projectId: "userinfo-7f8f9",
-    storageBucket: "userinfo-7f8f9.appspot.com",
-    messagingSenderId: "914637440469"
-}
-firebase.initializeApp(config)
+var defaultApp = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://userinfo-7f8f9.firebaseio.com"
+})
 
 var UUID = require('uuid/v1')
 
@@ -28,31 +24,26 @@ var server = net.createServer(socket => {
         var json_data = JSON.parse(decoder(data))
 
         if(json_data.action == 'SIGN_UP') {
-            firebase.auth().createUserWithEmailAndPassword(json_data.username, json_data.password)
-            .then(user => {
-                console.log('email', user.email)
-                console.log('name', user.displayName)
-                console.log('photoUrl', user.photoURL)
-                console.log('verified', user.emailVerified)
-                user.sendEmailVerification()
+            defaultApp.auth().createUser({
+                email: json_data.email,
+                emailVerified: false,
+                phoneNumber: "",
+                password: json_data.password,
+                displayName: "",
+                photoURL: "",
+                disabled: false
+            })
+            .then(userRecord => {
+                userRecord.sendEmailVerification()
                 .then(() => {
-                    var resp_data = {
-                        email: user.email,
-                        name: user.displayName,
-                        photoUrl: user.photoURL,
-                        email_verified: user.emailVerified
-                    }
-                    socket.write(JSON.stringify(resp_data))
+                    
                 })
                 .catch(e => {
                     console.error(e)
                 })
             })
             .catch(e => {
-                var errorCode = e.error
-                var errorMessage = e.message
-
-                console.log(errorCode, errorMessage)
+                console.error(e)
             })
         } else if(json_data.action == 'LOG_IN') {
             firebase.auth().signInWithEmailAndPassword(json_data.username, json_data.password)
