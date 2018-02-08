04dee25eb75586b6768bc92b2916db5ddb5a9078
jaekkim <jaekkim.rim.net>
2018-02-08 16:11:52 -0500

    add Crypto.js

--- Decoder.js
+++
@@ -1,7 +0,0 @@
-const atob = require('atob')
-
-export default function decoder(str) {
-    return decodeURIComponent(atob(str).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-}
--- Encoder.js
+++
@@ -1,7 +0,0 @@
-const btoa = require('btoa')
-
-export default function encoder(str) {
-    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, (match, p1) => {
-        return String.fromCharCode('0x' + p1)
-    }))
-}
